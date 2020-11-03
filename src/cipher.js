98fce27241efe9f65071e66539cf7f05211af597
paolahermosilla <paitohermosillacaro@gmail.com>
2020-11-02 22:46:06 -0300

    avance cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,29 @@
-const cipher = {
+function cifrar (txtMensaje , numeroDesplazamiento){
+    let resultado="";
+    let letras="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    numeroDesplazamiento= (numeroDesplazamiento % 26 + 26) % 26;
+    if (txtMensaje){
+        for (let i = 0; i<txtMensaje.length; i++){
+            if (letras.indexOf(txtMensaje[i])!=-1){
+                let posicion = (letras.indexOf(txtMensaje[i])+numeroDesplazamiento)%26;
+                resultado += letras[posicion];
+            }
+            else 
+            resultado += texto[i];
+        }
+
+        }
+          return resultado;
+    }
+
+
+    
+    
+    //cipher.encode(offset, string): offset es el número de posiciones que queremos mover a la derecha
+    // en el alfabeto y string el mensaje (texto) que queremos cifrar.
+ 
   
-};
+    //Formula  = (Posicion letra+Valor fijo )% residuo de 26
+
 
 export default cipher;
