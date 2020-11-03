f718d9f7bac858d36348afbdb8d6562b624a09de
paolahermosilla <paitohermosillacaro@gmail.com>
2020-11-03 00:15:14 -0300

    Correccion Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,27 @@
-function cifrar (txtMensaje , numeroDesplazamiento){
-    let resultado="";
-    let letras="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    numeroDesplazamiento= (numeroDesplazamiento % 26 + 26) % 26;
-    if (txtMensaje){
-        for (let i = 0; i<txtMensaje.length; i++){
-            if (letras.indexOf(txtMensaje[i])!=-1){
-                let posicion = (letras.indexOf(txtMensaje[i])+numeroDesplazamiento)%26;
-                resultado += letras[posicion];
+
+const cipher={
+    encode:function (txtMensaje , numeroDesplazamiento){
+        let resultado="";
+        let letras="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        numeroDesplazamiento= (numeroDesplazamiento % 26 + 26) % 26;
+        if (txtMensaje){
+            for (let i = 0; i<txtMensaje.length; i++){
+                
+               const ascii= txtMensaje.charCodeAt(i);
+               const asciimodificado= (ascii-65+numeroDesplazamiento)%26 + 65;
+            
+            resultado= resultado+String.fromCharCode(asciimodificado) 
             }
-            else 
-            resultado += texto[i];
+    
+            }
+              return resultado;
         }
+}
 
-        }
-          return resultado;
-    }
 
 
     
     
-    //cipher.encode(offset, string): offset es el número de posiciones que queremos mover a la derecha
-    // en el alfabeto y string el mensaje (texto) que queremos cifrar.
- 
-  
-    //Formula  = (Posicion letra+Valor fijo )% residuo de 26
+  export default cipher;
 
 
-export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import cipher from './cipher.js';
 
 function onClickEncriptar() {
-
+        debugger
         //1.OBTENER EL TEXTO(DOCUMENT.GET...IDCAJA CERRAR PARENTESIS Y PONER .VALUE) ASIGNARLO A UNA VARIABLE ,EXTRAER EL VALUE DE LA CAJA DE TEXTO
         const txtMensaje = document.getElementById("txtMensaje").value;
         
@@ -9,7 +9,7 @@ function onClickEncriptar() {
         const desplazamiento = document.getElementById("numeroDesplazamiento").value;
 
         //3.INVOCAR LA FUNCION DE CIPHER (FALTA ESCRIBIRLA)EL VALOR QUE ME DEVUELVA CIPHER ASIGNARLO A UNA VARIABLE
-        const txtMensajeEncriptado= "esto deberia estar encriptado";
+        const txtMensajeEncriptado= cipher.encode(txtMensaje,desplazamiento );
         //4.ASIGNAR EL TEXTO ENCRIPTADO AL MENSAJE2
          document.getElementById("txtMensajeEncriptado").value=txtMensajeEncriptado;
         //5.DECENCRIPTAR ES HACER LO MISMO PERO AL REVES 
