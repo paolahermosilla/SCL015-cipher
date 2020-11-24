b2b15fda7153c930636104f77ff7ac4c9ab8e911
paolahermosilla <paitohermosillacaro@gmail.com>
2020-11-23 22:25:21 -0300

    lineas comentadas primera parte

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,24 @@
 
 const cipher = {
   encode: function (txtMensaje, numeroDesplazamiento) {
-
+debugger;
     let resultado = "";
 
     numeroDesplazamiento = (numeroDesplazamiento % 26 + 26) % 26;
     //estudiar ,ir decifrando por parte .
 
-    for (let i = 0; i < txtMensaje.length; i++) {
-      const ascii = txtMensaje.charCodeAt(i);
+    ///["H","O","L","A"]
+    ///[ 0,  1,  2,  3]
+
+    const largoTexto = txtMensaje.length;
+
+    for (let contador = 0; contador < largoTexto; contador++) {
+    
+      const ascii = txtMensaje.charCodeAt(contador);
+
       const asciimodificado = (ascii - 65 + numeroDesplazamiento) % 26 + 65;
       resultado = resultado + String.fromCharCode(asciimodificado)
     }
-
-
 
     return resultado;
 
